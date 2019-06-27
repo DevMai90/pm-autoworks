b64fa5f0b790c02b61fbc8b0b9c522926d321dab
DevMai90 <dmai90@yahoo.com>
2019-06-26 23:56:53 -0700

    Added margin break points for improved navbar responsiveness

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,82 +5,80 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark navbar-expand-md py-3" id="main-nav">
-        <div className="container">
-          <Link to="/" className="navbar-brand">
-            <img
-              src={require('../../images/pm-logo.png')}
-              alt="PM Autoworks"
-              style={{
-                height: 'auto',
-                maxHeight: '30px',
-                width: 'auto',
-                maxWidth: '100%'
-              }}
-              className="rounded img-thumbnail"
-            />
-          </Link>
-          <button
-            className="navbar-toggler custom-toggler collapsed"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarCollapse"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="navbar-collapse collapse" id="navbarCollapse">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item dropdown px-2">
-                <a
-                  href="#"
-                  className="nav-link dropdown-toggle text-warning"
-                  role="button"
-                  data-toggle="dropdown"
-                >
-                  Services
+        <Link to="/" className="navbar-brand ml-md-5">
+          <img
+            src={require('../../images/pm-logo.png')}
+            alt="PM Autoworks"
+            style={{
+              height: 'auto',
+              maxHeight: '30px',
+              width: 'auto',
+              maxWidth: '100%'
+            }}
+            className="rounded img-thumbnail"
+          />
+        </Link>
+        <button
+          className="navbar-toggler custom-toggler collapsed"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="navbar-collapse collapse" id="navbarCollapse">
+          <ul className="navbar-nav ml-auto mr-md-5">
+            <li className="nav-item dropdown px-2">
+              <a
+                href="#"
+                className="nav-link dropdown-toggle text-warning"
+                role="button"
+                data-toggle="dropdown"
+              >
+                Services
+              </a>
+              <div className="dropdown-menu bg-dark">
+                <a href="#" className="dropdown-item text-warning">
+                  Maintenance
                 </a>
-                <div className="dropdown-menu bg-dark">
-                  <a href="#" className="dropdown-item text-warning">
-                    Maintenance
-                  </a>
-                  <a href="#" className="dropdown-item text-warning">
-                    Repair
-                  </a>
-                  <a href="#" className="dropdown-item text-warning">
-                    Diagnostics
-                  </a>
-                  <a href="#" className="dropdown-item text-warning">
-                    Performance
-                  </a>
-                </div>
-              </li>
-              <li className="nav-item dropdown px-2">
-                <a
-                  href="#"
-                  className="nav-link dropdown-toggle text-warning"
-                  role="button"
-                  data-toggle="dropdown"
-                >
-                  About Us
+                <a href="#" className="dropdown-item text-warning">
+                  Repair
                 </a>
-                <div className="dropdown-menu bg-dark">
-                  <a href="#" className="dropdown-item text-warning">
-                    About The Shop
-                  </a>
-                  <a href="#" className="dropdown-item text-warning">
-                    Meet The Team
-                  </a>
-                </div>
-              </li>
-              <li className="nav-item px-2">
-                <a
-                  href="#"
-                  className="nav-link btn btn-outline-warning text-warning px-2"
-                >
-                  Contact Us
+                <a href="#" className="dropdown-item text-warning">
+                  Diagnostics
                 </a>
-              </li>
-            </ul>
-          </div>
+                <a href="#" className="dropdown-item text-warning">
+                  Performance
+                </a>
+              </div>
+            </li>
+            <li className="nav-item dropdown px-2">
+              <a
+                href="#"
+                className="nav-link dropdown-toggle text-warning"
+                role="button"
+                data-toggle="dropdown"
+              >
+                About Us
+              </a>
+              <div className="dropdown-menu bg-dark">
+                <a href="#" className="dropdown-item text-warning">
+                  About The Shop
+                </a>
+                <a href="#" className="dropdown-item text-warning">
+                  Meet The Team
+                </a>
+              </div>
+            </li>
+            <li className="nav-item px-2">
+              <a
+                href="#"
+                className="nav-link btn btn-outline-warning text-warning px-2"
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     );
