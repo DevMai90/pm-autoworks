90d97cb4058f64f0d3a4244614dedd567ef65ab2
DevMai90 <dmai90@yahoo.com>
2019-06-28 10:51:21 -0700

    Add basename prop to router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
           <Navbar />
           <Switch>
